5fe56e76e9266dfdef18a6bbd9847b25b38a9420
shankarjatin <shankarjatin1005@gmail.com>
2026-09-21 00:10:35 +0530

    done

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     },
     webpack(config) {
       config.module.rules.push({
-        test: /\.(mp4|webm|ogg|swf|avi|jpg)$/,
+        test: /\.(mp4|webm|ogg|swf|avi)$/,
         type: 'asset/resource',
         generator: {
           filename: 'static/media/[name].[hash][ext]',
